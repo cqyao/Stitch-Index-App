9bce643edbed1f41971cd55a6423530465f080a5
t41y0u <vuthaiduong0607@gmail.com>
2024-07-30 01:22:54 +1000

    Added missing fonts for Android

--- StitchIndexApp/app/_layout.tsx
+++ StitchIndexApp/app/_layout.tsx
@@ -1,10 +1,30 @@
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Inter: require("../assets/fonts/Inter-Regular.ttf"),
+    Lato: require("../assets/fonts/Lato-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
-      <Stack.Screen  
-        name="index" 
+      <Stack.Screen
+        name="index"
         options={{
           // Hide the header for this route
           headerShown: false,
