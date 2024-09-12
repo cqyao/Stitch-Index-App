094d1c4b8b026f2622d717dbbbc4a2ae7da2f629
cqyao <timothyjt96@gmail.com>
2024-09-12 11:27:10 +1000

    Added forum posts, fixed dashboard shortcuts styling, removed yarn package manager (use npm)

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, View, FlatList, ViewToken} from 'react-native';
-import React, {useState} from 'react';
+import React from 'react';
 import Animated, {
     useSharedValue,
     useAnimatedScrollHandler,
@@ -25,55 +25,10 @@ const OnboardingScreen = () => {
         }
     };
 
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isSignUp, setIsSignUp] = useState(false); // Add a state to switch between sign up and login
-
-  const handleSignUp = () => {
-    if (!email || !password) {
-      console.error("Email and password are required");
-      return;
-    }
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed up
-          const user = userCredential.user;
-          console.log("User Signed Up: ", user.email);
-          router.push({
-            pathname: "./dashboard",
-          });
-        })
-        .catch((error) => {
-          console.error("Error signing up:", error);
-        });
-  };
-
-  const handleLogin = () => {
-    if (!email || !password) {
-      console.error("Email and password are required");
-      return;
-    }
-    signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Logged in
-          const user = userCredential.user;
-          console.log("Logged In With: ", user.email);
-          router.push({pathname: "./dashboard"});
-        })
-        .catch((error) => {
-          console.error("Error signing in:", error);
-        });
-  };
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log("User Logged In: ", user.email);
-        router.push("./dashboard");
-      } else {
-        console.log("User Logged Out");
-      }
+    const onScroll = useAnimatedScrollHandler({
+        onScroll: event => {
+            x.value = event.contentOffset.x;
+        },
     });
 
     return (
