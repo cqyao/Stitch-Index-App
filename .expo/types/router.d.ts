6da2e6cfba47d4cd3c84723bd7a13ec3cc4d07e6
NoahFieri <kyroap@gmail.com>
2024-10-03 17:49:02 +1000

    Added firstName, Lastname & Email to Async Storage. Fixed vulnerability that allowed the user to get into the dashboard after singing out

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/CreatePost` | `/_sitemap` | `/appointment` | `/calendar` | `/courses` | `/createpost` | `/dashboard` | `/patient` | `/post` | `/research` | `/search` | `/signIn` | `/signup`;
+      StaticRoutes: `/` | `/_sitemap` | `/appointment` | `/calendar` | `/courses` | `/createpost` | `/dashboard` | `/patient` | `/post` | `/research` | `/search` | `/signIn` | `/signup`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
